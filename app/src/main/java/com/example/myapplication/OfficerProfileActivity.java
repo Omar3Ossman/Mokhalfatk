061002b861d193a62946bceeaaec9d6805fa109b
Omar3Ossman <18p1498@eng.asu.edu.eg>
2023-05-26 05:02:08 +0200

    Creation of Police Officer Profile Screen and updating user profile screen

--- app/src/main/java/com/example/myapplication/OfficerProfileActivity.java
+++ app/src/main/java/com/example/myapplication/OfficerProfileActivity.java
@@ -1,4 +1,42 @@
 package com.example.myapplication;
 
-public class OfficerProfileActivity {
+import static com.example.myapplication.R.id.scanLPR;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Camera;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OfficerProfileActivity extends AppCompatActivity {
+    ImageView backButton;
+    TextView scanLPR;
+
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_officer_profile);
+
+        backButton = (ImageView) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OfficerProfileActivity.this, SignInOfficerActivity.class);
+                startActivity(intent);
+            }
+        });
+        scanLPR = findViewById(R.id.scanLPR);
+        scanLPR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OfficerProfileActivity.this, Camera.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
