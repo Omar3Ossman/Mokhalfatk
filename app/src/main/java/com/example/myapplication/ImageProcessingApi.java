2d76851ca4c7fbc1269c13d42fd402e3230c7185
Nour Abdelghany <93491631+nabdelghany00@users.noreply.github.com>
2023-06-19 23:41:11 +0300

    Integration is done

--- app/src/main/java/com/example/myapplication/ImageProcessingApi.java
+++ app/src/main/java/com/example/myapplication/ImageProcessingApi.java
@@ -1,5 +1,9 @@
 package com.example.myapplication;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -10,5 +14,32 @@ import retrofit2.http.Part;
 public interface ImageProcessingApi {
     @Multipart
     @POST("process_image")
-    Call<ResponseBody> processImage(@Part MultipartBody.Part image);
+    Call<LicensePlateData> processImage(@Part MultipartBody.Part image);
+}
+
+
+class LicensePlateData {
+    @SerializedName("image_url")
+    private String imageUrl;
+
+    @SerializedName("license_plate_numbers")
+    private List<String> licensePlateNumbers;
+
+    // Add getters and setters for the fields
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<String> getLicensePlateNumbers() {
+        return licensePlateNumbers;
+    }
+
+    public void setLicensePlateNumbers(List<String> licensePlateNumbers) {
+        this.licensePlateNumbers = licensePlateNumbers;
+    }
 }
